46b3f5495e6779fc004435da13db9b5450275351
elzneik <104824463+elzneik@users.noreply.github.com>
2022-07-05 15:22:05 +0200

    add DELETE for user degistration

--- index.js
+++ index.js
@@ -246,7 +246,20 @@ app.delete("/users/:id/:movieTitle", (req, res) => {
     }
 });
 
+// DELETE
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
 
+    let user = users.find( user => user.id == id); 
+
+    if(user) {
+       users = users.filter( user => user.id != id);
+       // res.json(users);
+        res.status(200).send("User ${id} has been deleted.");
+    } else {
+        res.status(400).send("No such user!");
+    }
+});
 
 
 
