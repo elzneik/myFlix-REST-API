4c2b7aab824cfd9f10a3e6741cfb91473bc409b0
elzneik <104824463+elzneik@users.noreply.github.com>
2022-07-15 08:34:13 +0200

    update index.js and models.js

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+    Title: {type:String, required:true},
+    Description: {type:String, required:true},
+    Genre: {
+        Name: String,
+        Description: String
+    }
+    Director: {
+        Name: String,
+        Bio: String,
+        Birthyear: String,
+        Deathyear: String
+    }
+    Actors: [String],
+    ImageURL: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type:String, required:true},
+    Password: {type:String, required:true},
+    Email: {type:String, required:true},
+    Birthday: Date,
+    FavoriteMovie: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}],
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
