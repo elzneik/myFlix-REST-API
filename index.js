0fd3d49f2a029638632c7c8304c6215ea72bc465
elzneik <104824463+elzneik@users.noreply.github.com>
2022-08-03 20:12:47 +0200

    update

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.post("/users", [
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashedPassword(req.body.Password);
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({Username: req.body.Username}) // search for user with username, exist
         .then((user) => {
             if (user) {
